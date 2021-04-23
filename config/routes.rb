90ed78d08ea0ef0d5841421909a0434412e296d5
Ubuntu <ubuntu@ip-172-31-6-52.us-east-2.compute.internal>
2021-04-23 18:58:03 +0000

    CRUD operations and adding before action

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'bienvenida', to: 'home#index'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update', as: :article
+  delete 'articles/:id', to: 'articles#destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
